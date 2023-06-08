45dea91231587675a571e93e820a9b3aac8cddb1
Andres Palacio <andres.palacio@utp.edu.co>
2023-06-07 19:59:07 -0500

    backend-update:     git add .     git commit -m "backend update"     git push origin main

---
+++ dept_info.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos Estadisticos</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <?php $dept = $_POST["dept"];
+        $jsonResponse = file_get_contents("https://www.datos.gov.co/resource/ji8i-4anb.json?\$select=departamento&\$where=c_digo_departamento=".$dept);
+        $data = json_decode($jsonResponse, true);
+    ?>
+    <style>
+        #columnchart_material, #chart_div, #curve_chart{
+            width: 75%;
+            height: 500px;
+            margin: auto;
+            padding-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.html">INICIO</a></li>
+                <li><a href="departamentos.html" class="active">DEPARTAMENTOS</a></li>
+                <li><a href="info_nacional.html">INFO. NACIONAL</a></li>
+                <li><a href="contacto.html">CONTACTO</a></li>
+                <li><div class="search-container">
+                    <input type="text" id="search-input" placeholder="Buscar departamento...">
+                    <button id="search-button">Buscar</button>
+                </div></li>
+            </ul>
+        </nav>
+        
+    </header>
+
+    <h1>Información de <?php echo $data[0]["departamento"]; ?></h1>
+
+    <div id="columnchart_material"></div>
+    <br><hr>
+    <div id="curve_chart"></div>
+    <br><hr>
+    <div id="chart_div"></div>
+
+    <script type="text/javascript">
+        var dept = <?php echo $dept; ?>;
+    </script>
+    <script type="text/javascript" src="graphics.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Escolaridad en Colombia</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <?php
     <header>
     <nav>
         <ul>
             <li><a href="index.html" class="active">INICIO</a></li>
-            <li><a href="dep_prueba.html">DEPARTAMENTOS</a></li>
+            <li><a href="departamentos.html">DEPARTAMENTOS</a></li>
             <li><a href="info_nacional.html">INFO. NACIONAL</a></li>
             <li><a href="contacto.html">CONTACTO</a></li>
         </ul>
@@ -22,6 +22,5 @@
     <!-- Resto del contenido de la página -->
     <h1>Bienvenido a mi página web</h1>
     <p>Contenido de la página...</p>
-    ?>
 </body>
 </html>
